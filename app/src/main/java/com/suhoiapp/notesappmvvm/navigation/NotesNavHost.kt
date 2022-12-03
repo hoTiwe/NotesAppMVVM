1b989541a52e0f943b62f44446b10546d14ac35a
hoTiwe <zerftiv125@gmail.com>
2022-12-03 23:02:25 +0300

    ouh..

---
+++ app/src/main/java/com/suhoiapp/notesappmvvm/navigation/NotesNavHost.kt
@@ -0,0 +1,15 @@
+package com.suhoiapp.notesappmvvm.navigation
+
+import androidx.compose.runtime.Composable
+
+sealed class NavRoute(val route: String) {
+    object Start: NavRoute("start_screen")
+    object Main: NavRoute("main_screen")
+    object Add: NavRoute("add_screen")
+    object Note: NavRoute("note_screen")
+}
+
+@Composable
+fun NotesNavHost() {
+
+}
