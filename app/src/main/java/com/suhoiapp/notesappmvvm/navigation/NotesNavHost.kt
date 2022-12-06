9e3cb4d9254760151ab26fbeda79c9a91dba764e
hoTiwe <zerftiv125@gmail.com>
2022-12-06 10:49:56 +0300

    add sample for navigation

--- app/src/main/java/com/suhoiapp/notesappmvvm/navigation/NotesNavHost.kt
+++ app/src/main/java/com/suhoiapp/notesappmvvm/navigation/NotesNavHost.kt
@@ -1,6 +1,13 @@
 package com.suhoiapp.notesappmvvm.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.suhoiapp.notesappmvvm.screens.Add
+import com.suhoiapp.notesappmvvm.screens.Main
+import com.suhoiapp.notesappmvvm.screens.Note
+import com.suhoiapp.notesappmvvm.screens.Start
 
 sealed class NavRoute(val route: String) {
     object Start: NavRoute("start_screen")
@@ -11,5 +18,14 @@ sealed class NavRoute(val route: String) {
 
 @Composable
 fun NotesNavHost() {
+    val navController = rememberNavController()
+    
+    NavHost(navController = navController, startDestination = NavRoute.Start.route){
+        composable(NavRoute.Start.route) { Start(navController = navController)}
+        composable(NavRoute.Main.route) { Main(navController = navController) }
+        composable(NavRoute.Add.route) { Add(navController = navController) }
+        composable(NavRoute.Note.route) { Note(navController = navController) }
+
+    }
 
 }
